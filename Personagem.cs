5c6196bee66db1cd55a6143fece6765b99125c2d
lLuskzl <31415342+lLuskzl@users.noreply.github.com>
2017-11-24 22:39:50 -0200

    Add files via upload

---
+++ Personagem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    public float velocidade;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Movimentacao();
+    }
+
+    void Movimentacao()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.Translate(Vector2.up * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.Translate(Vector2.down * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+    }
+}
---
+++ TrocarMapa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrocarMapa : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.root.CompareTag("Player"))
+        {
+            Application.LoadLevel("Lab");
+        }
+    }
+
+}
